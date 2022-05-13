0451246dde51ff60cb8dc31d1df1fffab3a175e4
Caitlin Stevenson <caitlinstevenson@Caitlins-Air.attlocal.net>
2022-05-13 17:19:39 -0500

    final commit

--- README.md
+++ README.md
@@ -2,15 +2,33 @@
 
 ## Objectives
 
-In this project, I 
+In this project, I created a weather dashboard web application that allows a user to input a city into the weather search form and receive the current and five day weather forecast for that location.
+
+## Tech Stack
+
+HTML
+CSS
+JavaScript
+Third Party API (Open Weather API)
+Local Storage
+
+## Installation
+
+To launch this application locally begin by cloning the repo down from the GitHub Repo [weather_dashboard](https://github.com/caitlinscodes/weather_dashboard). Once cloned right click on the index.html and select "Open with Live Server".
 
 ## Screenshot of Final Page
 
-![Screenshot of final webpage](./images/pending)
+![Screenshot of final webpage](./assets/images/finalpage1.png)
+
+![Screenshot of final webpage](./assets/images/finalpage2.png)
 
 ## Link To The Final Page
 
 [Link to final webpage](https://caitlinscodes.github.io/weather_dashboard/)
+
+## Link to GitHub Repo
+
+[Link to Repo](https://github.com/caitlinscodes/weather_dashboard)
 
 ## License
 
--- js/script.js
+++ js/script.js
@@ -1,13 +1,14 @@
 // Global Variables
 var searchHistory = JSON.parse(localStorage.getItem('history')) ?? []
+
 $(document).ready(function () {
     var searchBtn = $('#citySearch');
     var apiKey = "fe26f57c2c8868b05e39afb09fead7ee";
     const prevList = document.getElementById("list")
 
 
-    searchBtn.on("click", function (e) {
-        e.preventDefault()
+    searchBtn.on("click", function (event) {
+        event.preventDefault()
         var searchValue = $("#search").val()
         searchHistory.push(searchValue)
         localStorage.setItem("history", JSON.stringify(searchHistory))
@@ -16,10 +17,9 @@ $(document).ready(function () {
         getCurrentWeather(searchValue)
 
         searchHistory.forEach((item => {
-            //console.log(searchHistory)
-            let historyList = document.createElement('li');
-            historyList.textContent = `City: ${item}`;
-            prevList.appendChild(historyList)
+            let history = document.createElement('li');
+            history.textContent = `City: ${item}`;
+            prevList.appendChild(history)
         }))
     })
 
@@ -33,15 +33,13 @@ $(document).ready(function () {
         };
 
         const fiveDayUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coords.lat}&lon=${coords.lon}&exclude=current,minutely,hourly,alerts&appid=${apiKey}&units=imperial`
-        let data5Day = await (await fetch(fiveDayUrl)).json()
-
-        console.log("data from api", data)
+        let fiveDayData = await (await fetch(fiveDayUrl)).json()
 
         let cityName = data.name;
         let temp = data.main.temp;
         let wind = data.wind.speed;
         let humiditiy = data.main.humidity;
-        let uvi = data5Day.daily[0].uvi
+        let uvi = fiveDayData.daily[0].uvi
 
         let card = $("<div class='card'>");
         let cardHeader = $("<div class='card-header'>");
@@ -51,22 +49,21 @@ $(document).ready(function () {
         let windEl = $("<p class='card-text'>").text("Wind Speed: " + wind + " MPH")
         let humidEl = $("<p class='card-text'>").text("Humidity: " + humiditiy + "%")
         let nameEl = $("<p class='card-title'>").text(cityName)
-        let bgColor = "green"
-        if (uvi >= 6 && uvi <= 10) bgColor = "yellow"
-        if (uvi >= 11) bgColor = "red"
+        let bgColor = "#CEBECF"
+        if (uvi >= 6 && uvi <= 10) bgColor = "#D59A4B"
+        if (uvi >= 11) bgColor = "#CD0CCF"
         let uviEl = $("<p class='card-text'>")
             .text(`UV Index: ${uvi}`)
             .css({
                 "color": bgColor,
-                "background-color": "black"
+                "background-color": "#506B2C"
             })
         cardHeader.append(nameEl);
         cardBody.append(tempEl, windEl, humidEl, currentIcon, uviEl)
         card.append(cardHeader, cardBody)
         $("#currentWeather").append(card)
-        console.log("FIVEDAY DATA", data)
 
-        data5Day.daily
+        fiveDayData.daily
             .slice(1, 6)
             .forEach((day) => {
                 let temp = day.temp.day;
@@ -87,141 +84,4 @@ $(document).ready(function () {
             })
     }
 })
-
-
-
-
-// Old Code
-// var searchInput = "";
-// var latitude = "";
-// var longitude = "";
-// var currentWeatherResults = document.getElementsByClassName(".search-results");
-
-// // Open Weather API Key: fe26f57c2c8868b05e39afb09fead7ee
-// // Geo-Coding API = http://api.openweathermap.org/geo/1.0/direct?q={city name},{state code},{country code}&limit={limit}&appid={fe26f57c2c8868b05e39afb09fead7ee}
-// // Current Weather API = https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid=fe26f57c2c8868b05e39afb09fead7ee
-// // 5 Day API = api.openweathermap.org/data/2.5/forecast/daily?lat={lat}&lon={lon}&cnt=5&appid=fe26f57c2c8868b05e39afb09fead7ee
-
-// // Global Functions
-// // document.getElementById("#searchBtn").addEventListener("click", myFunction);
-// $("#searchBtn").on("click", myFunction);
-
-
-
-// function myFunction(event){
-//   event.preventDefault();
-//   searchInput = $("#search-input").val();
-//   console.log(searchInput);
-
-//   citySearchConverter();
-//   currentWeatherApi();
-// };
-
-// // APIs
-// function citySearchConverter(){
-//   fetch("http://api.openweathermap.org/geo/1.0/direct?q=" + searchInput + "&limit=5&appid=fe26f57c2c8868b05e39afb09fead7ee")
-//   .then(function(response){
-//     return response.json()
-//   })
-//   .then( function(data){
-//     console.log(data)
-    
-//     for (let i=0; i < data.length; i++){
-//       latitude = data[0].lat;
-//       longitude = data[0].lon;
-//       console.log(latitude, longitude);
-//       localStorage.setItem("latitude", latitude);
-//       localStorage.setItem("longitude", longitude);
-//     }
-
-
-//   })
-//   .catch(function(error){
-//     console.log(error)
-//   });
-// }
-
-// function currentWeatherApi(){
-//   latitude = JSON.parse(localStorage.getItem("latitude"));
-//   longitude = JSON.parse(localStorage.getItem("longitude", longitude));
-
-//   fetch("https://api.openweathermap.org/data/2.5/weather?lat=" + latitude + "&lon=" + longitude + "&appid=fe26f57c2c8868b05e39afb09fead7ee")
-//     .then(function(response){
-//       return response.json()
-//     })
-//     .then(function(data){
-//       console.log(data)
-//         for (let i = 0; i < data.length; i++) {
-//           //Creating elements
-//           var cityName = document.createElement('h2');
-//           var date = document.createElement('<p>');
-//           var temp = document.createElement('<li>');
-//           var humidity = document.createElement('<li>');
-//           var windSpeed = document.createElement('li');
-//           // var uvIdx = document.createElement('<li>');
-//           console.log("elements created");
-
-
-//           //Setting the text of the elements.
-//           cityName.textContent = data[i].name;
-//           date.textContent = data[i].date; // This should us momentJS (?)
-//           temp.textContent = data[i].temp;
-//           humidity.textContent = data[i].humidity;
-//           windSpeed.textContent = data[i].speed;
-//           // uvIdx.textContent = data[i].
-//           console.log("data pulled");
-          
-
-//           //Append to the html
-//           currentWeatherResults.append(cityName);
-//           currentWeatherResults.append(date);
-//           currentWeatherResults.append(temp);
-//           currentWeatherResults.append(humidity);
-//           currentWeatherResults.append(windSpeed);
-//           console.log("items appended");
-//     }
-//     // .catch(function(error){
-//     //   console.log(error)
-//     // });
-//   });
-
-
-//currently broken - Need another API key?
-// function fiveDayWeatherApi(){
-//   fetch("https://api.openweathermap.org/data/2.5/forecast/daily?lat=35&lon=139&cnt=5&appid=fe26f57c2c8868b05e39afb09fead7ee")
-//     .then(function(response){
-//       return response.json();
-//     })
-//     .then( function(data){
-//       console.log(data)
-//     })
-//     .catch(function(error){
-//       console.log(error)
-//     });
-// }
-
-//User can search for a city
-
-
-
-
-//User is presented with current weather conditions
-
-
-  // Current weather view includes; City Name, Date (momentjs?), icon representing weather, temperature, humidity, wind speed, and UV index.
-
-
-  // When user views the UV index they are presented with a color that indicates if conditions are favorable, moderate, or severe.
-
-
-//User is also presented with 5 day forecast
-
-
-  // Display includes; date, weather conditions icon, temperature, wind speed, and humidity.
-
-
-//City is saved in local storage and can be referenced in sidebar
-
-
-  //When user selects city from search history they can view the information again.
 
